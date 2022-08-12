403a8a7961779b99821fd1b8995730ad11cfdef3
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-11 21:04:28 -0700

    done with the assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+    def initialize (word)
+        @word = word
+    end
+
+    def match (array)
+        return array.select { |input| match_solo(input)}
+        # match_array = []
+        # array.select { |input| match_solo(input)}
+        #    if match_solo(input)
+        #         match_array.push(input)
+        #    end
+        # end
+        # return match_array
+    end
+
+    def match_solo (string) 
+        string = string.chars
+        word_chars = @word.chars
+        return string.sort == word_chars.sort
+    end
+end
+
+listen = Anagram.new("listen")
+puts listen.match(%w[enlist google inlets banana])
