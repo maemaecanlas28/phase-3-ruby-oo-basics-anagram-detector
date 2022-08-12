1a89d0c295f4a1fc0ac671b2856c7ac5881add6e
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-11 21:03:12 -0700

    commit tree 7f9e2c13630a5f7e3b73b65016fe57b8797f8f0a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    def initialize (word)
+        @word = word
+    end
+
+    def match (array)
+        return array.select { |input| match_solo(input)}
+    end
+
+    def match_solo (string) 
+        string = string.chars
+        word_chars = @word.chars
+        return string.sort == word_chars.sort
+    end
+end
+
+listen = Anagram.new("listen")
+puts listen.match(%w[enlist google inlets banana])
